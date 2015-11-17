dcd60cb5d770586b980b9b98f7972c6689d26df7
Niko Weh <niko.weh@gmail.com>
2015-11-17 13:10:54 +1000

    Cleaned up XML 'templates'

--- lib/elixlsx.ex
+++ lib/elixlsx.ex
@@ -98,44 +98,14 @@ defmodule Elixlsx do
       <diagonal/>
     </border>
   </borders>
-  <cellStyleXfs count="20">
-    <xf numFmtId="164" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="true" applyAlignment="true" applyProtection="true">
-      <alignment horizontal="general" vertical="bottom" textRotation="0" wrapText="false" indent="0" shrinkToFit="false"/>
-      <protection locked="true" hidden="false"/>
-    </xf>
-    <xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="2" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="2" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="43" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="41" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="44" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="42" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
-    <xf numFmtId="9" fontId="1" fillId="0" borderId="0" applyFont="true" applyBorder="false" applyAlignment="false" applyProtection="false"/>
+  <cellStyleXfs count="1">
+    <xf borderId="0" fillId="0" fontId="0" numFmtId="0"/>
   </cellStyleXfs>
   <cellXfs count="1">
-    <xf numFmtId="164" fontId="0" fillId="0" borderId="0" xfId="0" applyFont="false" applyBorder="false" applyAlignment="false" applyProtection="false">
-      <alignment horizontal="general" vertical="bottom" textRotation="0" wrapText="false" indent="0" shrinkToFit="false"/>
-      <protection locked="true" hidden="false"/>
-    </xf>
+    <xf borderId="0" fillId="0" fontId="0" numFmtId="0" xfId="0"/>
   </cellXfs>
-  <cellStyles count="6">
-    <cellStyle name="Normal" xfId="0" builtinId="0" customBuiltin="false"/>
-    <cellStyle name="Comma" xfId="15" builtinId="3" customBuiltin="false"/>
-    <cellStyle name="Comma [0]" xfId="16" builtinId="6" customBuiltin="false"/>
-    <cellStyle name="Currency" xfId="17" builtinId="4" customBuiltin="false"/>
-    <cellStyle name="Currency [0]" xfId="18" builtinId="7" customBuiltin="false"/>
-    <cellStyle name="Percent" xfId="19" builtinId="5" customBuiltin="false"/>
+  <cellStyles count="1">
+    <cellStyle builtinId="0" name="Normal" xfId="0"/>
   </cellStyles>
 </styleSheet>
 """	
@@ -148,20 +118,13 @@ defmodule Elixlsx do
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
   <fileVersion appName="Calc"/>
-  <workbookPr backupFile="false" showObjects="all" date1904="false"/>
-  <workbookProtection/>
   <bookViews>
-    <workbookView showHorizontalScroll="true" showVerticalScroll="true" showSheetTabs="true" xWindow="0" yWindow="0" windowWidth="16384" windowHeight="8192" tabRatio="992" firstSheet="0" activeTab="0"/>
+    <workbookView activeTab="0"/>
   </bookViews>
   <sheets>
     <sheet name="Sheet1" sheetId="1" state="visible" r:id="rId2"/>
   </sheets>
   <calcPr iterateCount="100" refMode="A1" iterate="false" iterateDelta="0.001"/>
-  <extLst>
-    <ext xmlns:loext="http://schemas.libreoffice.org/" uri="{7626C862-2A13-11E5-B345-FEFF819CDC9F}">
-      <loext:extCalcPr stringRefSyntax="Unspecified"/>
-    </ext>
-  </extLst>
 </workbook>
 """
 		}
@@ -180,6 +143,7 @@ defmodule Elixlsx do
 		}
 	end
 
+
 	def get_xl_worksheets_dir(data) do
 		[{'xl/worksheets/sheet1.xml',
 			~S"""
@@ -190,28 +154,19 @@ defmodule Elixlsx do
   </sheetPr>
   <dimension ref="A1"/>
   <sheetViews>
-    <sheetView windowProtection="false" showFormulas="false" showGridLines="true" showRowColHeaders="true" showZeros="true" rightToLeft="false" tabSelected="true" showOutlineSymbols="true" defaultGridColor="true" view="normal" topLeftCell="A1" colorId="64" zoomScale="100" zoomScaleNormal="100" zoomScalePageLayoutView="100" workbookViewId="0">
-      <selection pane="topLeft" activeCell="A2" activeCellId="0" sqref="A2"/>
+    <sheetView workbookViewId="0">
+      <selection activeCell="A1" sqref="A1"/>
     </sheetView>
   </sheetViews>
   <sheetFormatPr defaultRowHeight="12.8"/>
-  <cols>
-    <col collapsed="false" hidden="false" max="1025" min="1" style="0" width="11.5204081632653"/>
-  </cols>
   <sheetData>
-    <row r="1" customFormat="false" ht="12.8" hidden="false" customHeight="false" outlineLevel="0" collapsed="false">
+    <row r="1">
       <c r="A1" s="0" t="s">
         <v>0</v>
       </c>
     </row>
   </sheetData>
-  <printOptions headings="false" gridLines="false" gridLinesSet="true" horizontalCentered="false" verticalCentered="false"/>
   <pageMargins left="0.7875" right="0.7875" top="1.05277777777778" bottom="1.05277777777778" header="0.7875" footer="0.7875"/>
-  <pageSetup paperSize="1" scale="100" firstPageNumber="1" fitToWidth="1" fitToHeight="1" pageOrder="downThenOver" orientation="portrait" usePrinterDefaults="false" blackAndWhite="false" draft="false" cellComments="none" useFirstPageNumber="true" horizontalDpi="300" verticalDpi="300" copies="1"/>
-  <headerFooter differentFirst="false" differentOddEven="false">
-    <oddHeader>&amp;C&amp;"Times New Roman,Regular"&amp;12&amp;A</oddHeader>
-    <oddFooter>&amp;C&amp;"Times New Roman,Regular"&amp;12Page &amp;P</oddFooter>
-  </headerFooter>
 </worksheet>
 			"""
 			}]
--- libreoffice_test_script.sh
+++ libreoffice_test_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mix
 elixir -pa _build/dev/lib/elixlsx/ebin create_file.exs
 libreoffice --convert-to empty.pdf empty.xlsx
 exit $?
