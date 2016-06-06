be0318050ca01ec3eb8a80d63337fe8fea31b5c8
Niko Weh <niko.weh@gmail.com>
2016-06-06 23:06:43 +1000

    Version bump and ex_doc

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Elixlsx.Mixfile do
 
   def project do
     [app: :elixlsx,
-     version: "0.0.1",
+     version: "0.0.3",
      elixir: "~> 1.1",
      package: package,
      description: "a writer for XLSX spreadsheet files",
@@ -20,7 +20,8 @@ defmodule Elixlsx.Mixfile do
     [
       {:excheck, "~> 0.3", only: :test},
       {:triq, github: "krestenkrab/triq", only: :test},
-      {:credo, "~> 0.1.9", only: [:dev, :test]}
+      {:credo, "~> 0.1.9", only: [:dev, :test]},
+      {:ex_doc, "~> 0.11", only: [:dev]}
     ]
   end
 
