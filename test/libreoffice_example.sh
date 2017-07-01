45b5132a54181d2c76a30bb64533914519c43a7d
Niko Weh <niko.weh@gmail.com>
2017-07-01 19:00:54 +1000

    Rename test filename to 'example'

--- test/libreoffice_example.sh
+++ test/libreoffice_example.sh
@@ -4,7 +4,7 @@ test_dir=`dirname $0`
 pushd $test_dir/../
 mix
 elixir -pa _build/dev/lib/elixlsx/ebin example.exs
-libreoffice --convert-to empty.pdf empty.xlsx
+libreoffice --convert-to example.pdf example.xlsx
 rc=$?
 popd
 
