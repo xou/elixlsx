b49a322338fb199cf576ba65a5a9e53d4b095bdb
Charles <CharlesOkwuagwu@users.noreply.github.com>
2017-07-10 09:27:04 +0100

    Update xml_templates.ex

--- lib/elixlsx/xml_templates.ex
+++ lib/elixlsx/xml_templates.ex
@@ -171,6 +171,8 @@ defmodule Elixlsx.XMLTemplates do
         {"n", to_string(x)}
       x when is_binary(x) ->
         {"s", to_string(StringDB.get_id wci.stringdb, x)}
+      %Decimal{} = x ->
+        {"n", Decimal.to_string(x)}
       :empty ->
         {:empty, :empty}
       true ->
