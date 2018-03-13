6e9b73d5c3a58a2f7a742ef98de08685252865bf
Nikolai Weh <niko.weh@gmail.com>
2018-03-12 20:35:02 -0400

    Fix test warnings for elixir 1.5 (#62)

--- test/util_test.exs
+++ test/util_test.exs
@@ -5,7 +5,7 @@ defmodule ExCheck.UtilTest do
   alias Elixlsx.Util
 
   property :enc_dec do
-    for_all x in such_that(x in int when x >= 0) do
+    for_all x in such_that(x in int() when x >= 0) do
       implies x >= 0 do
         Util.decode_col(Util.encode_col(x)) == x
       end
