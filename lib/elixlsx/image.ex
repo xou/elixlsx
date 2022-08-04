43727b6b4244e5419a90a79dcc8dc88bd9a24664
Lukasz D <lukasz.dacz@llinformatics.com>
2022-08-04 16:14:47 +0200

    handle image

---
+++ lib/elixlsx/image.ex
@@ -0,0 +1,72 @@
+defmodule Elixlsx.Image do
+  alias Elixlsx.Image
+
+  @moduledoc ~S"""
+  Structure for excel drawing files.
+  - x_offset: integer
+  - y_offset: integer
+  - x_scale: float
+  - y_scale: float
+  - positioning: atom (:absolute, :oneCell, :twoCell)
+  - width: integer
+  - height: integer
+  """
+
+  defstruct file_path: "",
+            type: "image/png",
+            extension: "png",
+            rowidx: 0,
+            colidx: 0,
+            x_offset: 0,
+            y_offset: 0,
+            x_scale: 1,
+            y_scale: 1,
+            positioning: :twoCell,
+            width: 1,
+            height: 1
+
+  @type t :: %Image{
+          file_path: String.t(),
+          type: String.t(),
+          extension: String.t(),
+          rowidx: integer,
+          colidx: integer,
+          x_offset: integer,
+          y_offset: integer,
+          x_scale: float,
+          y_scale: float,
+          positioning: atom,
+          width: integer,
+          height: integer
+        }
+
+  @doc """
+  Create an image struct based on opts
+  """
+  def new(file_path, rowidx, colidx, opts \\ []) do
+    {ext, type} = image_type(file_path)
+
+    %Image{
+      file_path: file_path,
+      type: type,
+      extension: ext,
+      rowidx: rowidx,
+      colidx: colidx,
+      x_offset: Keyword.get(opts, :x_offset, 0),
+      y_offset: Keyword.get(opts, :y_offset, 0),
+      x_scale: Keyword.get(opts, :x_scale, 1),
+      y_scale: Keyword.get(opts, :y_scale, 1),
+      positioning: Keyword.get(opts, :positioning, :twoCell),
+      width: Keyword.get(opts, :width, 1),
+      height: Keyword.get(opts, :height, 1)
+    }
+  end
+
+  defp image_type(file_path) do
+    case Path.extname(file_path) do
+      ".jpg" -> {"jpg", "image/jpeg"}
+      ".jpeg" -> {"jpeg", "image/jpeg"}
+      ".png" -> {"png", "image/png"}
+    end
+  end
+end
